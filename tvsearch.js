3ec598154eb6aec3cb8edd3ba2ec81f8627984e9
NumaanisCoder <nomannaeem985@gmail.com>
2022-09-09 15:22:46 +0530

    first commit

---
+++ tvsearch.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log("Submited")
+    const user =  form.elements.query.value;
+    fetch(`https://api.tvmaze.com/search/shows?q=${user}`)
+    .then((res)=>{
+        res.json()
+        .then((response)=>{
+            const heading = document.querySelector('#heading');
+            heading.innerText = response[0].show.name;
+            const image = document.querySelector('#photo');
+            image.src = response[0].show.image.medium;
+            const rating = document.querySelector('#rating');
+            rating.innerText = "Rating: "+response[0].show.rating.average;
+            const rel = document.querySelector('#reld');
+            rel.innerText = "First episode date: "+response[0].show.premiered;
+            const endd = document.querySelector('#endd');
+            if(response[0].show.ended == null){
+                endd.innerText = "Last episode date: Still going.."
+            }else{
+                endd.innerText = "Last episode date: " +response[0].show.ended;
+            }
+            const para = document.querySelector('#detail');
+            para.innerHTML = response[0].show.summary;
+            console.log(response)
+            document.getElementById("info").style.display = "block";
+        })
+        .catch((e)=>{
+            const heading = document.querySelector('#heading');
+            heading.innerText = "Please inter valid show name";
+            document.getElementById("info").style.display = "none";
+        })
+    })
+})
